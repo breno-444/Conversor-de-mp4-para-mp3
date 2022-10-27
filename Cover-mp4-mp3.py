6929c1bef3d0a5254b5a24ed81f6098c2daf7d71
breno <brenorezende.4457@aluno.una.br>
2022-10-27 19:36:38 -0300

    commit inicial

---
+++ Cover-mp4-mp3.py
@@ -0,0 +1,21 @@
+'''
+  bibliotecas para serem instaladas
+  pip install moviepy
+  pip install glob
+'''
+import os
+from moviepy.editor import *
+import glob
+
+lista_mp4 = glob.glob('C:\Videos\*.mp4')
+#print(lista_mp4)
+
+for video in lista_mp4:
+  print('Lendo arquivo mp4')
+  print('-'*20)
+  mp4 = VideoFileClip(os.path.join(video))
+  nome_mp3 = video[:-4]+'.mp3'
+  print('Convertendo para mp3')
+  mp4.audio.write_audiofile(os.path.join(nome_mp3))
+  print('-'*20)
+  print('Converteu mp4 ' + video + ' para mp3 ' + nome_mp3)
